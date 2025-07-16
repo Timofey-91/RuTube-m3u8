3af64973ff37b6064f8dd18e23f9d0612d8e00f4
Timofey <50114291+Timofey-91@users.noreply.github.com>
2025-07-16 23:37:56 +0500

    Update rutube.py

--- rutube.py
+++ rutube.py
@@ -2,7 +2,7 @@ import requests
 import re
 
 def get_stream_link(video_id):
-    url = f"https://rutube.ru/api/video/live/{video_id}/"
+    url = f"https://rutube.ru/api/play/options/{video_id}/"
     headers = {"User-Agent": "Mozilla/5.0"}
     res = requests.get(url, headers=headers)
     res.raise_for_status()
