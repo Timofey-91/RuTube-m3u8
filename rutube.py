d3764af93849eb988fea8bf1958872f8c83850e8
Timofey <50114291+Timofey-91@users.noreply.github.com>
2025-07-16 23:04:26 +0500

    Create rutube.py

---
+++ rutube.py
@@ -0,0 +1,10 @@
+import requests
+import re
+
+def get_stream_link(video_id):
+    url = f"https://rutube.ru/api/video/live/{video_id}/"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    res = requests.get(url, headers=headers)
+    res.raise_for_status()
+    data = res.json()
+    return data.get("m3u8", "")
